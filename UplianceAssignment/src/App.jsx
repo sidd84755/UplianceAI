38f7c819c4946224411d52b4855f2654bc7237d0
sidd84755 <sidd84755@gmail.com>
2025-02-26 21:58:37 +0530

    Initial Commit

---
+++ UplianceAssignment/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { AuthProvider } from './components/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+
+const theme = createTheme();
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            {/* <Route path="/login" element={<LoginPage />} /> */}
+            <Route path="/" element={
+              <PrivateRoute>
+                  <Routes>
+                    {/* <Route path="/counter" element={<Counter />} />
+                    <Route path="/form" element={<UserForm />} />
+                    <Route path="/dashboard" element={<Dashboard />} /> */}
+                  </Routes>
+                
+              </PrivateRoute>
+            } />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
